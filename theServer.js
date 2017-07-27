57903c9d01af32d5c0d49dc4bf84a5948c73bdc9
chasingSublimity <blakemsager@gmail.com>
2017-07-27 12:24:40 -0500

    minor refactor

--- theServer.js
+++ theServer.js
@@ -4,12 +4,8 @@ const port = process.argv[2];
 
 const date = new Date();
 
-// console.log(port);
-// console.log(`${date.getFullYear()}-${date.getMonth()}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`);
-
 var server = net.createServer((socket) => {
-	socket.write(`${date.getFullYear()}-${'0' + (date.getMonth() + 1)}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}\n`);
-	socket.end();
+	socket.end(`${date.getFullYear()}-${'0' + (date.getMonth() + 1)}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}\n`);
 });
 
 server.listen(port);
