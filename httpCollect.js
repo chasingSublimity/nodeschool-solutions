f5d0ad74822ee914269aa2854e8ed20dd95143ae
chasingSublimity <blakemsager@gmail.com>
2017-07-27 11:28:32 -0500

    complete httpCollect

---
+++ httpCollect.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const url = process.argv[2];
+
+http.get(url, res => {
+	res.setEncoding('utf8');
+
+	let finishedData = [];
+
+	res.on('data', data => {
+		finishedData.push(data);
+	});
+
+	res.on('end', () => {
+		console.log(finishedData.join('').length);
+		console.log(finishedData.join(''));
+	});
+
+	res.on('error', console.error);
+}).on('error', console.error);
